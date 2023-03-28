4cf2b806ec48ad6e0a3662fefe1d6f5b3693e11c
doodleoo3 <106701127+doodleoo3@users.noreply.github.com>
2023-03-28 10:38:10 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -159,7 +159,6 @@ done
 
 read -p "What base directory should the node use (defaults to ~/shardeum): " NODEHOME
 NODEHOME=${NODEHOME:-~/shardeum}
-
 if read -t 100000 -sp "What priv_key node will use (priv_key from Metamask): " PRIV_KEY; then
 echo -e "\nSecret passphrase = '$PRIV_KEY'"
 else
@@ -204,7 +203,7 @@ cd ${NODEHOME} &&
 touch ./.env
 cat >./.env <<EOL
 APP_IP=auto
-APP_SEEDLIST=${APPSEEDLIST}
+EXISTING_ARCHIVERS=[{"ip":"18.194.3.6","port":4000,"publicKey":"758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3"},{"ip":"139.144.19.178","port":4000,"publicKey":"840e7b59a95d3c5f5044f4bc62ab9fa94bc107d391001141410983502e3cde63"},{"ip":"139.144.43.47","port":4000,"publicKey":"7af699dd711074eb96a8d1103e32b589e511613ebb0c6a789a9e8791b2b05f34"},{"ip":"72.14.178.106","port":4000,"publicKey":"2db7c949632d26b87d7e7a5a4ad41c306f63ee972655121a37c5e4f52b00a542"}]
 APP_MONITOR=${APPMONITOR}
 DASHPASS=${DASHPASS}
 DASHPORT=${DASHPORT}
@@ -262,7 +261,6 @@ echo "Starting image. This could take a while..."
 #Do not indent
 if [ $RUNDASHBOARD = "y" ]
 then
-
 cat <<EOF
   To use the Web Dashboard:
     1. Note the IP address that you used to connect to the node. This could be an external IP, LAN IP or localhost.
